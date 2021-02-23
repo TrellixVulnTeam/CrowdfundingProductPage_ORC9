fba18560f326cd4e050add6f6f54c998c672b3d6
PawanJS <contactpawanjs@gmail.com>
2021-02-23 10:34:20 +0530

    Added First section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict";
+const bookmarkBtn = document.querySelector(".js-bookmark");
+
+// Applying changes to bookmark button
+function bookmarked() {
+  bookmarkBtn.classList.add("btn--bookmarked");
+  //   changing icon
+  bookmarkBtn
+    .querySelector(".js-bookmarked")
+    .setAttribute("href", "images/sprite.svg#icon-bookmarked");
+
+  // changing text
+  bookmarkBtn.querySelector(".js-bookmark-text").innerText = "Bookmarked";
+}
+
+// Event Handlers
+bookmarkBtn.addEventListener("click", bookmarked);
