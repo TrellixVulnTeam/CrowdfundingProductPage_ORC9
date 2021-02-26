13ffb0cf101f33040849b5067224452067b9b33b
PawanJS <contactpawanjs@gmail.com>
2021-02-26 10:26:05 +0530

    Added bookmark button

--- js/main.js
+++ js/main.js
@@ -3,14 +3,25 @@ const bookmarkBtn = document.querySelector(".js-bookmark");
 
 // Applying changes to bookmark button
 function bookmarked() {
-  bookmarkBtn.classList.add("btn--bookmarked");
-  //   changing icon
-  bookmarkBtn
-    .querySelector(".js-bookmarked")
-    .setAttribute("href", "images/sprite.svg#icon-bookmarked");
+  bookmarkBtn.classList.toggle("btn--bookmarked");
 
-  // changing text
-  bookmarkBtn.querySelector(".js-bookmark-text").innerText = "Bookmarked";
+  if (bookmarkBtn.classList.contains("btn--bookmarked")) {
+    //   changing icon
+    bookmarkBtn
+      .querySelector(".js-bookmarked")
+      .setAttribute("href", "images/sprite.svg#icon-bookmarked");
+
+    // changing text
+    bookmarkBtn.querySelector(".js-bookmark-text").innerText = "Bookmarked";
+  } else {
+    //   changing icon
+    bookmarkBtn
+      .querySelector(".js-bookmarked")
+      .setAttribute("href", "images/sprite.svg#icon-bookmark");
+
+    // changing text
+    bookmarkBtn.querySelector(".js-bookmark-text").innerText = "Bookmark";
+  }
 }
 
 // Event Handlers
