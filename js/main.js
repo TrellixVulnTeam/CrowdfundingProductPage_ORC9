0e828714a575d6e09a75d9199a363d33d6024228
PawanJS <contactpawanjs@gmail.com>
2021-03-03 20:23:28 +0530

    Added abilty to increase amount

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 "use strict";
 const bookmarkBtn = document.querySelector(".js-bookmark");
 const pledgeBtn = document.querySelectorAll(".js-pledge-btn");
-const modalBox = document.querySelectorAll(".js-modal-box");
-const modal = document.querySelector(".js-modal");
 const btnRadio = document.querySelectorAll(".btn--radio");
-const openModalBtn = document.querySelector(".js-open-modal");
+const modal = document.querySelector(".js-modal");
+const modalBox = document.querySelectorAll(".js-modal-box");
+const successModal = document.querySelector(".js-modal-success");
+const openModalBtn = document.querySelectorAll(".js-open-modal");
 const closeModalBtn = document.querySelector(".js-close-modal");
+const closeSuccessModalBtn = document.querySelector(".js-close-success");
+const openSuccessModalBtn = document.querySelectorAll(".js-open-success");
 const overlay = document.querySelector(".js-overlay");
+const progressBar = document.querySelector(".js-progress");
+const amount = document.querySelector(".js-amount");
+const backers = document.querySelector(".js-backers");
+const amountGiven = document.querySelectorAll(".js-amount-given");
+
+let backerNumber = 5007;
+let amountCollected = 89914;
 
 // Applying changes to bookmark button
 function bookmarked() {
@@ -55,16 +65,43 @@ function activeModal() {
   });
 }
 
+// Incrementing backers
+function incrementBackers() {
+  backerNumber++;
+  backers.textContent = backerNumber;
+}
+// Incrementing amount
+function incrementAmount() {
+  amountGiven.forEach((amt) => {
+    const parent = amt.parentElement.parentElement.parentElement;
+    if (parent.classList.contains("modal__box--active")) {
+      amountCollected += Number(amt.value);
+      amount.textContent = amountCollected;
+    }
+    amt.value = "";
+  });
+}
+
 // Open modal
 function openModal() {
   modal.classList.add("modal--active");
   overlay.classList.add("modal--overlay-active");
 }
 
+// Open success Modal
+function openSuccessModal() {
+  successModal.classList.add("modal--active");
+}
+
 // Close modal
 function closeModal() {
   modal.classList.remove("modal--active");
   overlay.classList.remove("modal--overlay-active");
+}
+
+// close Success Modal
+function closeSuccessModal() {
+  successModal.classList.remove("modal--active");
 }
 
 // Event Handlers
@@ -78,7 +115,7 @@ modalBox.forEach((modal) => {
   });
 });
 
-openModalBtn.addEventListener("click", openModal);
+openModalBtn.forEach((btn) => btn.addEventListener("click", openModal));
 closeModalBtn.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
@@ -87,4 +124,18 @@ document.addEventListener("keydown", function (e) {
 });
 overlay.addEventListener("click", function (e) {
   closeModal();
+});
+
+openSuccessModalBtn.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    closeModal();
+    openSuccessModal();
+    incrementBackers();
+    incrementAmount();
+  })
+);
+
+closeSuccessModalBtn.addEventListener("click", () => {
+  closeSuccessModal();
 });
